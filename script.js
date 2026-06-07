d4055ec83c17d0315330ccb172013f3e842c120b
tambour-major <thomas.geninbrien@hotmail.ca>
2026-06-06 22:03:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
 fetch("texte.txt")
-  .then(response => response.text())
+  .then(response => {
+    console.log("STATUS:", response.status);
+    return response.text();
+  })
   .then(text => {
-
-    const container = document.getElementById("fragments");
-
-    const blocks = text.split("\n\n"); // paragraphes
-
-    blocks.forEach((block, i) => {
-
-      const p = document.createElement("p");
-      p.innerHTML = block.replace(/\n/g, "<br>");
-      container.appendChild(p);
-
-      if (i < blocks.length - 1) {
-        const spacer = document.createElement("div");
-        spacer.className = "spacer";
-        container.appendChild(spacer);
-      }
-
-    });
-
+    console.log("LONGUEUR TEXTE:", text.length);
+  })
+  .catch(err => {
+    console.error("ERREUR FETCH:", err);
   });
